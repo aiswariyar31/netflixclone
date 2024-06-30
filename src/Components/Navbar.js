1239cb3adce29ee44bf7b6a70471297672f91730
aiswariyar31 <k.aiswariyak@gmail.com>
2024-07-01 00:26:47 +0300

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png' alt=''/>
+      <img className='avatar' src='https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png' alt=''/> 
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3';
+export const API_KEY ='4320df79adf1e864aae978575638e395';
+export const imageUrl ='https://image.tmdb.org/t/p/original'
